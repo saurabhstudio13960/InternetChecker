49a7d53f41a458a45e51ed6826b8f6720688a694
RAVI-MEHTA\ravi <saurabhstudio13960@gmail.com>
2024-06-04 11:32:39 +0530

    Init code of the project

---
+++ Assets/InternetChecker/Script/Demo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour {
+
+	public void CheckInternetConn()
+	{		
+		InternetChecker.MyInternet += MyListener;
+		InternetChecker.ICInstance.StartInternetCheck ();
+	}
+
+	public void MyListener(bool isInternetAvailable) 
+	{
+		if (isInternetAvailable) 
+		{
+			Debug.Log ("Internet is Available");
+		}
+
+		else if (!isInternetAvailable) 
+		{
+			Debug.Log ("Internet is not Available");
+		}
+	}
+
+	void OnDisable()
+	{
+		InternetChecker.MyInternet -= MyListener;
+	}
+}
---
+++ Assets/InternetChecker/Script/InternetChecker.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+public class InternetChecker : MonoBehaviour
+{
+	private const bool allowCarrierDataNetwork = false;
+	private const string pingAddress = "8.8.8.8"; // Google Public DNS server
+	public const float waitingTime = 5.0f;
+
+	bool internetConnectBool;
+	private Ping ping;
+	private float pingStartTime;
+
+	public delegate void InternetCheckerD(bool InternetAvailable);
+	public static event InternetCheckerD MyInternet;
+
+	public static InternetChecker ICInstance;
+
+	void Start () 
+	{
+		ICInstance = new InternetChecker();
+		if (ICInstance != null) 
+		{
+			GameObject.Destroy(ICInstance);
+		}
+		else
+		{
+			ICInstance = this;
+		}	
+	}
+
+	public void StartInternetCheck()
+	{
+		bool internetPossiblyAvailable;
+		switch (Application.internetReachability)
+		{
+		case NetworkReachability.ReachableViaLocalAreaNetwork:
+			internetPossiblyAvailable = true;
+			break;
+		case NetworkReachability.ReachableViaCarrierDataNetwork:
+			//internetPossiblyAvailable = allowCarrierDataNetwork;
+			internetPossiblyAvailable = true;
+			break;
+		default:
+			internetPossiblyAvailable = false;
+			break;
+		}
+		if (!internetPossiblyAvailable)
+		{
+			InternetIsNotAvailable();
+			return;
+		}
+		ping = new Ping(pingAddress);
+		pingStartTime = Time.time;
+	}
+	
+	public void Update()
+	{
+		if (ping != null)
+		{
+			bool stopCheck = true;
+			if (ping.isDone)
+				InternetAvailable();
+			else if (Time.time - pingStartTime < waitingTime)
+				stopCheck = false;
+			else
+				InternetIsNotAvailable();
+			if (stopCheck)
+				ping = null;
+		}
+	}
+	
+	public void InternetIsNotAvailable()
+	{
+		if (MyInternet != null) 
+		{	
+			MyInternet (false);
+		}
+
+		internetConnectBool = false;
+	}
+	
+	public void InternetAvailable()
+	{
+		if (MyInternet != null)
+		{	
+			MyInternet (true);
+		}
+
+		internetConnectBool = true;
+	}
+
+	public void InternetCheck()
+	{
+		bool internetPossiblyAvailable;
+		switch (Application.internetReachability)
+		{
+			case NetworkReachability.ReachableViaLocalAreaNetwork:
+				internetPossiblyAvailable = true;
+				break;
+			case NetworkReachability.ReachableViaCarrierDataNetwork:
+			internetPossiblyAvailable = true;
+				break;
+			default:
+				internetPossiblyAvailable = false;
+				break;
+		}
+		if (!internetPossiblyAvailable)
+		{
+			InternetIsNotAvailable();
+			return;
+		}
+		ping = new Ping(pingAddress);
+		pingStartTime = Time.time;
+	}
+}
